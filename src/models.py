51d07d94f7dd4accdcb5e79261f9e4283dfffe57
Dans182 <dgaiteiro@gmail.com>
2022-05-01 19:08:15 +0000

    database

--- src/models.py
+++ src/models.py
@@ -1,12 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
+    
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(250))
+      
+class Character(db.Model):
+    __tablename__ = 'character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)    
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(250), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship(User)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    character = db.relationship(Character)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    planet = db.relationship(Planet)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.id"))
+    vehicle = db.relationship(Vehicle)
 
     def __repr__(self):
         return '<User %r>' % self.username
